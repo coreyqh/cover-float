c7bb8dc4155171c0fbd200b7d9856403b4a135c3
Corey Hickson <chickson@hmc.edu>
2025-11-10 02:02:11 -0800

    Extend B1 cg, factor out long bin lists into include files

--- coverage/covergroups/B1.svh
+++ coverage/covergroups/B1.svh
@@ -5,121 +5,25 @@ covergroup B1_cg (virtual coverfloat_interface CFI);
     FP_result_ops: coverpoint CFI.op {
         type_option.weight = 0;
         // all operations that produce (arbitrary) FP results
-        // `include "FP_result_op_bins.svh"
-            bins op_add    = {OP_ADD & ~32'hF};
-            bins op_sub    = {OP_SUB & ~32'hF};
-            bins op_mul    = {OP_MUL & ~32'hF};
-            bins op_div    = {OP_DIV & ~32'hF};
-            bins op_rem    = {OP_REM & ~32'hF};
-            bins op_qc     = {OP_QC & ~32'hF};
-            bins op_feq    = {OP_FEQ};
-            bins op_sc     = {OP_SC & ~32'hF};
-            bins op_flt    = {OP_FLT};
-            bins op_fle    = {OP_FLE};
-            bins op_min    = {OP_MIN & ~32'hF};
-            bins op_max    = {OP_MAX & ~32'hF};
-            bins op_csn    = {OP_CSN & ~32'hF};
-            bins op_fsgnj  = {OP_FSGNJ};
-            bins op_fsgnjn = {OP_FSGNJN};
-            bins op_fsgnjx = {OP_FSGNJX};
-            bins op_fma    = {OP_FMA & ~32'hF};
-            bins op_fmadd  = {OP_FMADD};
-            bins op_fmsub  = {OP_FMSUB};
-            bins op_fnmadd = {OP_FNMADD};
-            bins op_fnmsub = {OP_FNMSUB};
-            bins op_sqrt   = {OP_SQRT & ~32'hF};
+        `include "bins_templates/FP_result_op_bins.svh"
     }
 
     FP_src1_ops: coverpoint CFI.op {
         type_option.weight = 0;
         // all operations where the first operand is FP
-        // `include "FP_src1_op_bins.svh"
-
-        // 1 fp input
-        bins op_sqrt   = {OP_SQRT & ~32'hF};
-        bins op_cfi    = {OP_CFI & ~32'hF};
-        bins op_fcvtw  = {OP_FCVTW};
-        bins op_fcvtwu = {OP_FCVTWU};
-        bins op_fcvtl  = {OP_FCVTL};
-        bins op_fcvtlu = {OP_FCVTLU};
-        bins op_cff    = {OP_CFF & ~32'hF}; 
-        bins op_class  = {OP_CLASS & ~32'hF};
+        `include "bins_templates/FP_src1_op_bins.svh"
     }
 
     FP_src2_ops: coverpoint CFI.op {
         type_option.weight = 0;
         // all operations where the second operand is FP
-        // `include "FP_src2_op_bins.svh"
-
-        // 1 fp input
-        bins op_sqrt   = {OP_SQRT & ~32'hF};
-        bins op_cfi    = {OP_CFI & ~32'hF};
-        bins op_fcvtw  = {OP_FCVTW};
-        bins op_fcvtwu = {OP_FCVTWU};
-        bins op_fcvtl  = {OP_FCVTL};
-        bins op_fcvtlu = {OP_FCVTLU};
-        bins op_cff    = {OP_CFF & ~32'hF}; 
-        bins op_class  = {OP_CLASS & ~32'hF};
-
-        // 2 fp inputs
-        bins op_add    = {OP_ADD & ~32'hF};
-        bins op_sub    = {OP_SUB & ~32'hF};
-        bins op_mul    = {OP_MUL & ~32'hF};
-        bins op_div    = {OP_DIV & ~32'hF};
-        bins op_rem    = {OP_REM & ~32'hF};
-        bins op_qc     = {OP_QC & ~32'hF};
-        bins op_feq    = {OP_FEQ};
-        bins op_sc     = {OP_SC & ~32'hF};
-        bins op_flt    = {OP_FLT};
-        bins op_fle    = {OP_FLE};
-        bins op_min    = {OP_MIN & ~32'hF};
-        bins op_max    = {OP_MAX & ~32'hF};
-        bins op_csn    = {OP_CSN & ~32'hF};
-        bins op_fsgnj  = {OP_FSGNJ};
-        bins op_fsgnjn = {OP_FSGNJN};
-        bins op_fsgnjx = {OP_FSGNJX};
+        `include "bins_templates/FP_src2_op_bins.svh"
     }
 
     FP_src3_ops: coverpoint CFI.op {
         type_option.weight = 0;
         // all operations where the third operand is FP
-        // `include "FP_src3_op_bins.svh"
-
-        // 1 fp input
-        bins op_sqrt   = {OP_SQRT & ~32'hF};
-        bins op_cfi    = {OP_CFI & ~32'hF};
-        bins op_fcvtw  = {OP_FCVTW};
-        bins op_fcvtwu = {OP_FCVTWU};
-        bins op_fcvtl  = {OP_FCVTL};
-        bins op_fcvtlu = {OP_FCVTLU};
-        bins op_cff    = {OP_CFF & ~32'hF}; 
-        bins op_class  = {OP_CLASS & ~32'hF};
-
-        // 2 fp inputs
-        bins op_add    = {OP_ADD & ~32'hF};
-        bins op_sub    = {OP_SUB & ~32'hF};
-        bins op_mul    = {OP_MUL & ~32'hF};
-        bins op_div    = {OP_DIV & ~32'hF};
-        bins op_rem    = {OP_REM & ~32'hF};
-        bins op_qc     = {OP_QC & ~32'hF};
-        bins op_feq    = {OP_FEQ};
-        bins op_sc     = {OP_SC & ~32'hF};
-        bins op_flt    = {OP_FLT};
-        bins op_fle    = {OP_FLE};
-        bins op_min    = {OP_MIN & ~32'hF};
-        bins op_max    = {OP_MAX & ~32'hF};
-        bins op_csn    = {OP_CSN & ~32'hF};
-        bins op_fsgnj  = {OP_FSGNJ};
-        bins op_fsgnjn = {OP_FSGNJN};
-        bins op_fsgnjx = {OP_FSGNJX};
-    
-        // 3 fp inputs
-        bins op_fma    = {OP_FMA & ~32'hF};
-        bins op_fmadd  = {OP_FMADD};
-        bins op_fmsub  = {OP_FMSUB};
-        bins op_fnmadd = {OP_FNMADD};
-        bins op_fnmsub = {OP_FNMSUB};
-
+        `include "bins_templates/FP_src3_op_bins.svh"
     }
 
     F32_src_fmt: coverpoint CFI.operandFmt == FMT_SINGLE {
@@ -136,130 +40,22 @@ covergroup B1_cg (virtual coverfloat_interface CFI);
 
     F32_src1_basictypes: coverpoint CFI.a[31:0] {
         type_option.weight = 0;
-        bins pos0             = {32'h00000000};
-        bins neg0             = {32'h80000000};
-        bins pos1             = {32'h3f800000};
-        bins neg1             = {32'hbf800000};
-        bins pos1p5           = {32'h3fc00000};
-        bins neg1p5           = {32'hbfc00000};
-        bins pos2             = {32'h40000000};
-        bins neg2             = {32'hc0000000};
-        bins posminnorm       = {32'h00800000};
-        bins mnegminnorm      = {32'h80800000};
-        bins posmaxnorm       = {32'h7f7fffff};
-        bins negmaxnorm       = {32'hff7fffff};
-        bins posnorm          = {[32'h00800000:32'h7f7fffff]};
-        bins negnorm          = {[32'h80800000:32'hff7fffff]};
-        bins posmax_subnorm   = {32'h007fffff};
-        bins negmax_subnorm   = {32'h807fffff};
-        bins posmid_subnorm   = {32'h00400000};
-        bins negmid_subnorm   = {32'h80400000};
-        bins posmin_subnorm   = {32'h00000001};
-        bins negmin_subnorm   = {32'h80000001};
-        bins pos_subnorm      = {[32'h00000001:32'h007fffff]};
-        bins neg_subnorm      = {[32'h80000001:32'h807fffff]};
-        bins posinfinity      = {32'h7f800000};
-        bins neginfinity      = {32'hff800000};
-        bins posQNaN          = {[32'h7fc00000:32'h7fffffff]};
-        bins posSNaN          = {[32'h7f800001:32'h7fbfffff]};
-        bins negQNaN          = {[32'hffc00000:32'hffffffff]};
-        bins negSNaN          = {[32'hff800001:32'hffbfffff]};
+        `include "bins_templates/F32_basic_types_bins.svh"
     }
 
     F32_src2_basictypes: coverpoint CFI.b[31:0] {
         type_option.weight = 0;
-        bins pos0             = {32'h00000000};
-        bins neg0             = {32'h80000000};
-        bins pos1             = {32'h3f800000};
-        bins neg1             = {32'hbf800000};
-        bins pos1p5           = {32'h3fc00000};
-        bins neg1p5           = {32'hbfc00000};
-        bins pos2             = {32'h40000000};
-        bins neg2             = {32'hc0000000};
-        bins posminnorm       = {32'h00800000};
-        bins mnegminnorm      = {32'h80800000};
-        bins posmaxnorm       = {32'h7f7fffff};
-        bins negmaxnorm       = {32'hff7fffff};
-        bins posnorm          = {[32'h00800000:32'h7f7fffff]};
-        bins negnorm          = {[32'h80800000:32'hff7fffff]};
-        bins posmax_subnorm   = {32'h007fffff};
-        bins negmax_subnorm   = {32'h807fffff};
-        bins posmid_subnorm   = {32'h00400000};
-        bins negmid_subnorm   = {32'h80400000};
-        bins posmin_subnorm   = {32'h00000001};
-        bins negmin_subnorm   = {32'h80000001};
-        bins pos_subnorm      = {[32'h00000001:32'h007fffff]};
-        bins neg_subnorm      = {[32'h80000001:32'h807fffff]};
-        bins posinfinity      = {32'h7f800000};
-        bins neginfinity      = {32'hff800000};
-        bins posQNaN          = {[32'h7fc00000:32'h7fffffff]};
-        bins posSNaN          = {[32'h7f800001:32'h7fbfffff]};
-        bins negQNaN          = {[32'hffc00000:32'hffffffff]};
-        bins negSNaN          = {[32'hff800001:32'hffbfffff]};
+        `include "bins_templates/F32_basic_types_bins.svh"
     }
 
     F32_src3_basictypes: coverpoint CFI.c[31:0] {
         type_option.weight = 0;
-        bins pos0             = {32'h00000000};
-        bins neg0             = {32'h80000000};
-        bins pos1             = {32'h3f800000};
-        bins neg1             = {32'hbf800000};
-        bins pos1p5           = {32'h3fc00000};
-        bins neg1p5           = {32'hbfc00000};
-        bins pos2             = {32'h40000000};
-        bins neg2             = {32'hc0000000};
-        bins posminnorm       = {32'h00800000};
-        bins mnegminnorm      = {32'h80800000};
-        bins posmaxnorm       = {32'h7f7fffff};
-        bins negmaxnorm       = {32'hff7fffff};
-        bins posnorm          = {[32'h00800000:32'h7f7fffff]};
-        bins negnorm          = {[32'h80800000:32'hff7fffff]};
-        bins posmax_subnorm   = {32'h007fffff};
-        bins negmax_subnorm   = {32'h807fffff};
-        bins posmid_subnorm   = {32'h00400000};
-        bins negmid_subnorm   = {32'h80400000};
-        bins posmin_subnorm   = {32'h00000001};
-        bins negmin_subnorm   = {32'h80000001};
-        bins pos_subnorm      = {[32'h00000001:32'h007fffff]};
-        bins neg_subnorm      = {[32'h80000001:32'h807fffff]};
-        bins posinfinity      = {32'h7f800000};
-        bins neginfinity      = {32'hff800000};
-        bins posQNaN          = {[32'h7fc00000:32'h7fffffff]};
-        bins posSNaN          = {[32'h7f800001:32'h7fbfffff]};
-        bins negQNaN          = {[32'hffc00000:32'hffffffff]};
-        bins negSNaN          = {[32'hff800001:32'hffbfffff]};
+        `include "bins_templates/F32_basic_types_bins.svh"
     }
 
     F32_result_basictypes: coverpoint CFI.result[31:0] {
         type_option.weight = 0;
-        bins pos0             = {32'h00000000};
-        bins neg0             = {32'h80000000};
-        bins pos1             = {32'h3f800000};
-        bins neg1             = {32'hbf800000};
-        bins pos1p5           = {32'h3fc00000};
-        bins neg1p5           = {32'hbfc00000};
-        bins pos2             = {32'h40000000};
-        bins neg2             = {32'hc0000000};
-        bins posminnorm       = {32'h00800000};
-        bins mnegminnorm      = {32'h80800000};
-        bins posmaxnorm       = {32'h7f7fffff};
-        bins negmaxnorm       = {32'hff7fffff};
-        bins posnorm          = {[32'h00800000:32'h7f7fffff]};
-        bins negnorm          = {[32'h80800000:32'hff7fffff]};
-        bins posmax_subnorm   = {32'h007fffff};
-        bins negmax_subnorm   = {32'h807fffff};
-        bins posmid_subnorm   = {32'h00400000};
-        bins negmid_subnorm   = {32'h80400000};
-        bins posmin_subnorm   = {32'h00000001};
-        bins negmin_subnorm   = {32'h80000001};
-        bins pos_subnorm      = {[32'h00000001:32'h007fffff]};
-        bins neg_subnorm      = {[32'h80000001:32'h807fffff]};
-        bins posinfinity      = {32'h7f800000};
-        bins neginfinity      = {32'hff800000};
-        bins posQNaN          = {[32'h7fc00000:32'h7fffffff]};
-        bins posSNaN          = {[32'h7f800001:32'h7fbfffff]};
-        bins negQNaN          = {[32'hffc00000:32'hffffffff]};
-        bins negSNaN          = {[32'hff800001:32'hffbfffff]};
+        `include "bins_templates/F32_basic_types_bins.svh"
     }
 
     // main coverpoints
@@ -271,12 +67,34 @@ covergroup B1_cg (virtual coverfloat_interface CFI);
         B1_F32_result:     cross FP_result_ops, F32_result_basictypes,                                         F32_result_fmt;
     `endif // COVER_F32
 
-    // `ifdef COVER_F32
-    // `ifdef COVER_F64
-    //     B1_F32_F64_operand: cross 
-    //     B1_F64_F32_operand: cross
-    //     B1_F32_F64_result:  cross 
-    //     B1_F64_F32_result:  cross
-    // `endif 
-    // `endif // COVER_F32 && COVER_F64
+    `ifdef COVER_F64
+        B1_F64_1_operands: cross FP_src1_ops,   F64_src1_basictypes,                                           F64_src_fmt;
+        B1_F64_2_operands: cross FP_src2_ops,   F64_src1_basictypes, F64_src2_basictypes,                      F64_src_fmt;
+        B1_F64_3_operands: cross FP_src3_ops,   F64_src1_basictypes, F64_src2_basictypes, F64_src3_basictypes, F64_src_fmt;
+        B1_F64_result:     cross FP_result_ops, F64_result_basictypes,                                         F64_result_fmt;
+    `endif // COVER_F64
+
+    `ifdef COVER_F16
+        B1_F16_1_operands: cross FP_src1_ops,   F16_src1_basictypes,                                           F16_src_fmt;
+        B1_F16_2_operands: cross FP_src2_ops,   F16_src1_basictypes, F16_src2_basictypes,                      F16_src_fmt;
+        B1_F16_3_operands: cross FP_src3_ops,   F16_src1_basictypes, F16_src2_basictypes, F16_src3_basictypes, F16_src_fmt;
+        B1_F16_result:     cross FP_result_ops, F16_result_basictypes,                                         F16_result_fmt;
+    `endif // COVER_F16
+
+    `ifdef COVER_BF16
+        B1_BF16_1_operands: cross FP_src1_ops,   BF16_src1_basictypes,                                             BF16_src_fmt;
+        B1_BF16_2_operands: cross FP_src2_ops,   BF16_src1_basictypes, BF16_src2_basictypes,                       BF16_src_fmt;
+        B1_BF16_3_operands: cross FP_src3_ops,   BF16_src1_basictypes, BF16_src2_basictypes, BF16_src3_basictypes, BF16_src_fmt;
+        B1_BF16_result:     cross FP_result_ops, BF16_result_basictypes,                                           BF16_result_fmt;
+    `endif // COVER_BF16
+
+    
+    `ifdef COVER_F128
+        B1_F128_1_operands: cross FP_src1_ops,   F128_src1_basictypes,                                             F128_src_fmt;
+        B1_F128_2_operands: cross FP_src2_ops,   F128_src1_basictypes, F128_src2_basictypes,                       F128_src_fmt;
+        B1_F128_3_operands: cross FP_src3_ops,   F128_src1_basictypes, F128_src2_basictypes, F128_src3_basictypes, F128_src_fmt;
+        B1_F128_result:     cross FP_result_ops, F128_result_basictypes,                                           F128_result_fmt;
+    `endif // COVER_F128
+
+
 endgroup

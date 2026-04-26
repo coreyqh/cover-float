fdb4784f20f2d1e9465dd3c774f43e57b8bbec90
Corey Hickson <chickson@hmc.edu>
2026-04-26 15:34:11 -0700

    Add license header

--- coverage/coverfloat_interface.sv
+++ coverage/coverfloat_interface.sv
@@ -1,4 +1,19 @@
-interface coverfloat_interface; import coverfloat_pkg::*; // TODO: add params for covervector / DUT modes?
+// Copyright (C) 2025-26 Harvey Mudd College
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+interface coverfloat_interface; import coverfloat_pkg::*;
 
     // bit         clk;
 
@@ -8,7 +23,7 @@ interface coverfloat_interface; import coverfloat_pkg::*; // TODO: add params fo
 
     bit [7:0]  rm;
 
-    // bit [31:0]  enableBits; // legacy, not required for riscv TODO: consider having coverage based on these as a config option
+    // bit [31:0]  enableBits; // legacy, not required for riscv
 
     bit [127:0] a, b, c;
     bit [7:0]   operandFmt;

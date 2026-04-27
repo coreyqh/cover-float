00b503d809b623d2d211195d08beede86d20bcce
Corey Hickson <chickson@hmc.edu>
2026-04-27 03:38:43 -0700

    Remove solderpad, add full license text

--- coverage/covergroups/B16.svh
+++ coverage/covergroups/B16.svh
@@ -1,12 +1,12 @@
 // Copyright (C) 2025-26 Harvey Mudd College
 //
-// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+// SPDX-License-Identifier: Apache-2.0
 //
-// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file
-// except in compliance with the License, or, at your option, the Apache License version 2.0. You
-// may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// https://solderpad.org/licenses/SHL-2.1/
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, any work distributed under the
 // License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,

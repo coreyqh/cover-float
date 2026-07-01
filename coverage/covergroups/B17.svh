76711e3c685c57da29096013b01fad9779780b54
Ryan Wolk <rwolk@hmc.edu>
2026-07-01 13:32:25 -0700

    Update B17 Covergroup to Correct for Exponent Biases & Refine Impossible Cases
    Signed-off-by: Ryan Wolk <rwolk@hmc.edu>

--- coverage/covergroups/B17.svh
+++ coverage/covergroups/B17.svh
@@ -68,14 +68,18 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
             )
                 -
             (
-                (int'(get_product_exponent(CFI.a, CFI.b, FMT_HALF)) > int'(effective_exponent(CFI.c[F16_E_UPPER:F16_E_LOWER], FMT_HALF)))
+                (int'(get_product_exponent(CFI.a, CFI.b, FMT_HALF)) > int'(effective_exponent(CFI.c, FMT_HALF) + F16_EXP_BIAS))
                 ? int'(get_product_exponent(CFI.a, CFI.b, FMT_HALF))
-                : int'(effective_exponent(CFI.c[F16_E_UPPER:F16_E_LOWER], FMT_HALF))
+                : int'(effective_exponent(CFI.c, FMT_HALF) + F16_EXP_BIAS)
             )
         )
     {
         type_option.weight = 0;
-        bins cancel[] = {[-(2*F16_P + 1) : 1]};
+        // The paper says that [-(2p+1), 1] is the bounds, but I am unconvinced that the -2p and -(2p+1) cases
+        // are even possible. From the test suite that they have made available, the test count is consistent with
+        // not generating these cases.
+        // bins cancel[] = {[-(2*F16_P + 1) : 1]};
+        bins cancel[] = {[-(2*F16_P - 1) : 1]};
     }
 
     BF16_madd_cancellation: coverpoint
@@ -87,14 +91,18 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
             )
                 -
             (
-                (int'(get_product_exponent(CFI.a, CFI.b, FMT_BF16)) > int'(effective_exponent(CFI.c[BF16_E_UPPER:BF16_E_LOWER], FMT_BF16)))
+                (int'(get_product_exponent(CFI.a, CFI.b, FMT_BF16)) > int'(effective_exponent(CFI.c, FMT_BF16) + BF16_EXP_BIAS))
                 ? int'(get_product_exponent(CFI.a, CFI.b, FMT_BF16))
-                : int'(effective_exponent(CFI.c[BF16_E_UPPER:BF16_E_LOWER], FMT_BF16))
+                : int'(effective_exponent(CFI.c, FMT_BF16) + BF16_EXP_BIAS)
             )
         )
     {
         type_option.weight = 0;
-        bins cancel[] = {[-(2*BF16_P + 1) : 1]};
+        // The paper says that [-(2p+1), 1] is the bounds, but I am unconvinced that the -2p and -(2p+1) cases
+        // are even possible. From the test suite that they have made available, the test count is consistent with
+        // not generating these cases.
+        // bins cancel[] = {[-(2*BF16_P + 1) : 1]};
+        bins cancel[] = {[-(2*BF16_P - 1) : 1]};
     }
 
     F32_madd_cancellation: coverpoint
@@ -106,14 +114,18 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
             )
                 -
             (
-                (int'(get_product_exponent(CFI.a, CFI.b, FMT_SINGLE)) > int'(effective_exponent(CFI.c[F32_E_UPPER:F32_E_LOWER], FMT_SINGLE)))
+                (int'(get_product_exponent(CFI.a, CFI.b, FMT_SINGLE)) > int'(effective_exponent(CFI.c, FMT_SINGLE) + F32_EXP_BIAS))
                 ? int'(get_product_exponent(CFI.a, CFI.b, FMT_SINGLE))
-                : int'(effective_exponent(CFI.c[F32_E_UPPER:F32_E_LOWER], FMT_SINGLE))
+                : int'(effective_exponent(CFI.c, FMT_SINGLE) + F32_EXP_BIAS)
             )
         )
     {
         type_option.weight = 0;
-        bins cancel[] = {[-(2*F32_P + 1) : 1]};
+        // The paper says that [-(2p+1), 1] is the bounds, but I am unconvinced that the -2p and -(2p+1) cases
+        // are even possible. From the test suite that they have made available, the test count is consistent with
+        // not generating these cases.
+        // bins cancel[] = {[-(2*F32_P + 1) : 1]};
+        bins cancel[] = {[-(2*F32_P - 1) : 1]};
     }
 
     F64_madd_cancellation: coverpoint
@@ -125,14 +137,18 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
             )
                 -
             (
-                (int'(get_product_exponent(CFI.a, CFI.b, FMT_DOUBLE)) > int'(effective_exponent(CFI.c[F64_E_UPPER:F64_E_LOWER], FMT_DOUBLE)))
+                (int'(get_product_exponent(CFI.a, CFI.b, FMT_DOUBLE)) > int'(effective_exponent(CFI.c, FMT_DOUBLE) + F64_EXP_BIAS))
                 ? int'(get_product_exponent(CFI.a, CFI.b, FMT_DOUBLE))
-                : int'(effective_exponent(CFI.c[F64_E_UPPER:F64_E_LOWER], FMT_DOUBLE))
+                : int'(effective_exponent(CFI.c, FMT_DOUBLE) + F64_EXP_BIAS)
             )
         )
     {
         type_option.weight = 0;
-        bins cancel[] = {[-(2*F64_P + 1) : 1]};
+        // The paper says that [-(2p+1), 1] is the bounds, but I am unconvinced that the -2p and -(2p+1) cases
+        // are even possible. From the test suite that they have made available, the test count is consistent with
+        // not generating these cases.
+        // bins cancel[] = {[-(2*F64_P + 1) : 1]};
+        bins cancel[] = {[-(2*F64_P - 1) : 1]};
     }
 
     F128_madd_cancellation: coverpoint
@@ -144,14 +160,18 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
             )
                 -
             (
-                (int'(get_product_exponent(CFI.a, CFI.b, FMT_QUAD)) > int'(effective_exponent(CFI.c[F128_E_UPPER:F128_E_LOWER], FMT_QUAD)))
+                (int'(get_product_exponent(CFI.a, CFI.b, FMT_QUAD)) > int'(effective_exponent(CFI.c, FMT_QUAD) + F128_EXP_BIAS))
                 ? int'(get_product_exponent(CFI.a, CFI.b, FMT_QUAD))
-                : int'(effective_exponent(CFI.c[F128_E_UPPER:F128_E_LOWER], FMT_QUAD))
+                : int'(effective_exponent(CFI.c, FMT_QUAD) + F128_EXP_BIAS)
             )
         )
     {
         type_option.weight = 0;
-        bins cancel[] = {[-(2*F128_P + 1) : 1]};
+        // The paper says that [-(2p+1), 1] is the bounds, but I am unconvinced that the -2p and -(2p+1) cases
+        // are even possible. From the test suite that they have made available, the test count is consistent with
+        // not generating these cases.
+        // bins cancel[] = {[-(2*F128_P + 1) : 1]};
+        bins cancel[] = {[-(2*F128_P - 1) : 1]};
     }
 
     // Subnormal Exponent Coverpoint
@@ -205,35 +225,30 @@ covergroup B17_cg (virtual coverfloat_interface CFI);
     `ifdef COVER_F32
         B17_F32_subnormal_cancel: cross FMA_ops, F32_src_fmt, F32_madd_cancellation, F32_subnorm_exp, F32_subnormal {
             ignore_bins min_subnorm_carry = binsof(F32_subnorm_exp.negative_exp) intersect {F32_M_BITS-1} && binsof(F32_madd_cancellation.cancel) intersect {1};
-            ignore_bins min_subnorm_no_cancel = binsof(F32_subnorm_exp.negative_exp) intersect {F32_M_BITS-1} && binsof(F32_madd_cancellation.cancel) intersect {0};
         }
     `endif
 
     `ifdef COVER_F64
         B17_F64_subnormal_cancel: cross FMA_ops, F64_src_fmt, F64_madd_cancellation, F64_subnorm_exp, F64_subnormal {
             ignore_bins min_subnorm_carry = binsof(F64_subnorm_exp.negative_exp) intersect {F64_M_BITS-1} && binsof(F64_madd_cancellation.cancel) intersect {1};
-            ignore_bins min_subnorm_no_cancel = binsof(F64_subnorm_exp.negative_exp) intersect {F64_M_BITS-1} && binsof(F64_madd_cancellation.cancel) intersect {0};
         }
     `endif
 
     `ifdef COVER_F128
         B17_F128_subnormal_cancel: cross FMA_ops, F128_src_fmt, F128_madd_cancellation, F128_subnorm_exp, F128_subnormal {
             ignore_bins min_subnorm_carry = binsof(F128_subnorm_exp.negative_exp) intersect {F128_M_BITS-1} && binsof(F128_madd_cancellation.cancel) intersect {1};
-            ignore_bins min_subnorm_no_cancel = binsof(F128_subnorm_exp.negative_exp) intersect {F128_M_BITS-1} && binsof(F128_madd_cancellation.cancel) intersect {0};
         }
     `endif
 
     `ifdef COVER_F16
         B17_F16_subnormal_cancel: cross FMA_ops, F16_src_fmt, F16_madd_cancellation, F16_subnorm_exp, F16_subnormal {
             ignore_bins min_subnorm_carry = binsof(F16_subnorm_exp.negative_exp) intersect {F16_M_BITS-1} && binsof(F16_madd_cancellation.cancel) intersect {1};
-            ignore_bins min_subnorm_no_cancel = binsof(F16_subnorm_exp.negative_exp) intersect {F16_M_BITS-1} && binsof(F16_madd_cancellation.cancel) intersect {0};
         }
     `endif
 
     `ifdef COVER_BF16
         B17_BF16_subnormal_cancel: cross FMA_ops, BF16_src_fmt, BF16_madd_cancellation, BF16_subnorm_exp, BF16_subnormal {
             ignore_bins min_subnorm_carry = binsof(BF16_subnorm_exp.negative_exp) intersect {BF16_M_BITS-1} && binsof(BF16_madd_cancellation.cancel) intersect {1};
-            ignore_bins min_subnorm_no_cancel = binsof(BF16_subnorm_exp.negative_exp) intersect {BF16_M_BITS-1} && binsof(BF16_madd_cancellation.cancel) intersect {0};
         }
     `endif
 endgroup
